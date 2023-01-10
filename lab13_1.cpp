e8576459bcce27fb2d74d3c38ae01c316f31ef52
Yotsawat Sukinee <yotsawat2557@gmail.com>
2023-01-10 15:16:13 +0700

    finishe lab13_1 part1

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,21 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci(int x){
+    if(x==0) x=0;
+    if(x==1) x=1;
+    if(x>1){
+        int y = fibonacci(x-1)+fibonacci(x-2);
+        return y;
+    }
+    return x;
 }
